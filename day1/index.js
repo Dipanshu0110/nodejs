b05b2bbeb35af334dc8487ca68ffc69d02adf644
dipanshu0110 <dipanshuk55555@gmail.com>
2025-08-29 10:51:43 +0530

    initial commit

---
+++ day1/index.js
@@ -0,0 +1,9 @@
+//console.log("this is my first program")
+// const data=require("fs")
+// data.writeFileSync("hello.txt","hello shyam")
+// data.readFile("hello.txt","utf-8",(err,data)=>{
+//     console.log(data)
+// })
+
+const data=required("os")
+console.log(data.platform())
---
+++ day1/use.js
@@ -0,0 +1,2 @@
+const data=require("./module.js")
+console.log(data.sub(10,7))
